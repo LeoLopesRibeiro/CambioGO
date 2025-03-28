13c05f48da509d9693a4933006579405b172e8de
LeoLopes <leolopesribeiro05@gmail.com>
2025-03-28 08:53:05 -0300

    Home quase finalizada

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 import api from "./api.js";
 
-const data = await api("USD-BRL,EUR-BRL");
+const data = await api("USD-BRL,EUR-BRL,GBP-BRL");
+
 const display = document.querySelector("#cards");
 
 console.log(data);
@@ -10,9 +11,9 @@ display.innerHTML = Object.entries(data)
     console.log(key);
     // console.log(value.name);
     return `<div class="container"> 
-              <p>${value.name}</p>
-              <p>High: ${value.high}</p>
-              <p>Low: ${value.low}</p>
+              <p id="nome">${value.name}</p>
+              <p id="high">High: ${value.high}</p>
+              <p id="low">Low: ${value.low}</p>
             </div>`;
   })
   .join("");
@@ -20,24 +21,4 @@ display.innerHTML = Object.entries(data)
 // Object.entries(data).map(([key, value]) => {});
 
 //menu
-const btnMobile = document.getElementById('btn-mobile');
 
-function toggleMenu(event) {
-    if (event.type === 'touchstart') event.preventDefault();
-
-    const nav = document.getElementById('nav');
-
-    nav.classList.toggle('active');
-    const active = nav.classList.contains('active')
-    event.currentTarget.setAttribute('aria-expanded' , 'true')
-
-    if (active) {
-        event.currentTarget.setAttribute('aria-label' , 'Fechar Menu')
-    }
-    else{
-        event.currentTarget.setAttribute('aria-label' , 'Abrir Menu')
-    }
-}
-
-btnMobile.addEventListener('click' , toggleMenu);
-btnMobile.addEventListener('touchstart' , toggleMenu);
